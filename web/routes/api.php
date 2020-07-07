3ec3a6ddbf39df43f59552941e1b71f0303586a6
Sekiguchi Yuto <yutodazepa@icloud.com>
2020-07-07 17:19:51 +0900

    LoginApiTest追加

--- web/routes/api.php
+++ web/routes/api.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+Route::post('/login', 'Auth\LoginController@login')->name('login');
---
+++ web/tests/Feature/LoginApiTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\User;
+
+class LoginApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function ユーザーを認証して返却する()
+    {
+        $response = $this->json('POST', route('login'), [
+            'email'    => $this->user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200)
+                ->assertJson(['name' => $this->user->name]);
+            
+        $this->assertAuthenticatedAs($this->user);
+    }
+}
