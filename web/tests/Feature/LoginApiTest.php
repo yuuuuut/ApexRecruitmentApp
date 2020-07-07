6f67c2046a5989adb03ff267dfa27d06a5e6ed79
Sekiguchi Yuto <yutodazepa@icloud.com>
2020-07-07 17:26:09 +0900

    Models追加

--- web/tests/Feature/LoginApiTest.php
+++ web/tests/Feature/LoginApiTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-use App\User;
+use App\Models\User;
 
 class LoginApiTest extends TestCase
 {
