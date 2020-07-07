035b72a7002b2c2f789e2dc5b600b7c165fee8d0
Sekiguchi Yuto <yutodazepa@icloud.com>
2020-07-07 23:49:49 +0900

    LoginForm追加

--- web/resources/js/app.js
+++ web/resources/js/app.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 
 import router from './router'
+import Vuetify from 'vuetify';
+
+import 'vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.css'
 
 import App from './App.vue'
+
+Vue.use(Vuetify);
 
 new Vue({
   el: '#app',
   router,
+  vuetify: new Vuetify({
+    icons: {
+      iconfont: 'mdi',
+    },
+  }),
   components: { App },
   template: '<App />'
 })
--- web/resources/js/pages/Login.vue
+++ web/resources/js/pages/Login.vue
@@ -1,3 +1,82 @@
 <template>
-  <h1>Login</h1>
+  <v-app>
+    <v-card
+      class="mx-auto"
+      width="900px"
+      outlined>
+      <v-tabs
+        v-model="tab"
+        background-color="deep-purple accent-4"
+        center-active
+        grow
+        dark>
+        <v-tab
+          v-for="item in items"
+          :key="item.tab">
+          {{ item.tab }}
+        </v-tab>
+      </v-tabs>
+
+      <v-tabs-items v-model="tab">
+        <v-tab-item
+          v-for="item in items"
+          :key="item.tab">
+          <!-- ログインフォーム -->
+          <div v-show="item.tab === 'ログイン'">
+            <form @submit.prevent="login">
+              <v-text-field
+                class="mt-3 ml-10 mr-10"
+                v-model="loginForm.email"
+                :rules="emailRules"
+                label="メールアドレス"
+                hide-details="auto"></v-text-field>
+              <v-text-field
+                class="mt-5 ml-10 mr-10"
+                v-model="loginForm.password"
+                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="show1 ? 'text' : 'password'"
+                label="パスワード"
+                counter
+                @click:append="show1 = !show1"></v-text-field>
+
+              <button type="submit" class="button button--inverse">login</button>
+            </form>
+          </div>
+
+          <div v-show="item.tab === '新規登録'">
+            あ
+          </div>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      tab: null,
+      show1: false,
+      items: [
+        { tab: 'ログイン' },
+        { tab: '新規登録' },
+      ],
+      loginForm: {
+        email: '',
+        password: '',
+      },
+      //Validations
+      emailRules: [
+        v => !!v || 'メールアドレスを入力してください',
+        v => /.+@.+\..+/.test(v) || '無効なメールアドレスです',
+      ],
+    }
+  },
+  methods: {
+    login () {
+      console.log(this.loginForm)
+    }
+  }
+}
+</script>
